e714cab831e9e4eccb9581965a5537a11983d018
Luis Gutierrez <angelgcdev@gmail.com>
2024-11-11 19:20:10 -0400

    se corriguio la base de datos el archivo db.js para el puerto del .env, y se deshabilito el boton borrar reporte para los usuarios

--- src/db.js
+++ src/db.js
@@ -15,7 +15,7 @@ const pool = new pg.Pool({
   database: DB_DATABASE,
   port: DB_PORT,
   // Configuracion adicional para UTF-8
-  connectionString: `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_DATABASE}?client_encodeing=UTF8`
+  connectionString: `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_DATABASE}?client_encoding=UTF8`
 });
 
 export { pool };
